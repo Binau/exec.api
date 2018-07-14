a22c578c443c1fe6446ba8da87ac25fe6f025aa8
binau <aubin.roumillac@gmail.com>
2018-07-14 21:00:25 +0200

    Ajout du jeu de test mult

--- data/tests/base-js-mult/index.js
+++ data/tests/base-js-mult/index.js
@@ -1,6 +1,5 @@
 const printTestResult = require('./std.out').printTestResult;
 
-
 // Code injecté
 let mult =
     #CODE1;
@@ -9,7 +8,7 @@ let mult =
 let param = process.argv[2];
 if (!!param) {
     let data = JSON.parse(param);
-// Appel de la méthode detest
+    // Appel de la méthode detest
     let testResult = mult(data.val1, data.val2);
 // Inscription des résultats
     printTestResult(testResult);
